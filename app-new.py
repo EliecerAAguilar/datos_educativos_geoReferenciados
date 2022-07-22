180087d14ddd2bb51f4dc0fad7c73c9c74b43656
Kevin Gonzalez <keving@ayudinga.org>
2022-07-21 22:47:12 -0500

    grafica

--- app-new.py
+++ app-new.py
@@ -1,28 +1,86 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from turtle import color
+from dash import Dash, html, dcc, Input, Output
+import plotly.graph_objs as go
+import plotly.express as px
+import psycopg2
 import pandas as pd
-import numpy as np
-import dash_table
-import dash_bootstrap_components as dbc
+
+
+
+connection = psycopg2.connect(
+    database = "educacion",
+    user = "dashboard",
+    password = "BLHVvZwewtQVQ3",
+    host = "educationdb.postgres.database.azure.com",
+    port = 5432
+)
+
+app = Dash(__name__)
+
+
+
+
+
+app.layout = html.Div(
+    html.Div([
+        html.Div([
+            html.H1(children='Dashboard Educativo'),
+            html.H2(children='''Datos Basados en el nodo de Datos Abiertos de la Republica de Panama'''),
+            html.Div(id='my-div'),
+            ]),
+            
+            
+        html.Div(children=[
+            html.Label('Area'),
+            dcc.Dropdown(['URBANA', 'RURAL', 'INDIGENA'], 'URBANA' ,  id='area'),
+            html.Br(),
+            html.Label('Oferta Academica'),
+            dcc.Dropdown(['PRIMARIA TELEBÁSICA', 'PRIMARIA MULTIGRADO', 'PRIMARIA UNIGRADO', 'PRIMARIA TELEBASICA'], 'PRIMARIA UNIGRADO', id='oferta' )
+        ], style={'padding': 10, 'flex': 1, }),
+         
+        dcc.Graph(id='grafica-barra'), 
+
+    ]))
+
+
+
+@app.callback(
+    Output('grafica-barra', 'figure'),
+    Input('area', 'value'),
+    Input('oferta', 'value')
+)
+def update_chart(area , oferta):    
+    sql = f"""SELECT status,gender, sum(quantity) FROM school INNER JOIN level ON school.schoolid = level."schoolID" INNER JOIN result r on level."levelID" = r."levelID" WHERE school.area = '{area}' and school.program = '{oferta}' GROUP BY status, gender;"""
+    dat = pd.read_sql_query(sql, connection)
+    return px.bar(dat, x="status", y="sum", color="gender",  barmode="stack")
+
+
+
+
+
+
+
+
+
+
 # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv')
-df = pd.read_csv('pma_shp/final.csv', low_memory=False)
+#df = pd.read_csv('pma_shp/final.csv', low_memory=False)
 
 # Subset dataframe to show some specific columns in dash web app
-df1 = df[['PROVINCIA','CORREGIMIENTO', 'LAT', 'LONG']]
+#df1 = df[['PROVINCIA','CORREGIMIENTO', 'LAT', 'LONG']]
 
 # Find Lat Long center
-lat_center = sum(df['LAT']) / len(df['LAT'])
-long_center = sum(df['LONG']) / len(df['LONG'])
+#lat_center = sum(df['LAT']) / len(df['LAT'])
+#long_center = sum(df['LONG']) / len(df['LONG'])
 
 # Find Lat Long center
 # lat_center = sum(df['lat']) / len(df['lat'])
 # long_center = sum(df['long']) / len(df['long'])
 
-app = dash.Dash(external_stylesheets=['https://codepen.io/amyoshino/pen/jzXypZ.css'])
+#app = dash.Dash(external_stylesheets=['https://codepen.io/amyoshino/pen/jzXypZ.css'])
 # app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-app.title = 'Open Street Map'
-
+#app.title = 'Open Street Map'
+"""
 layout_map = dict(
     autosize=True,
     height=500,
@@ -83,6 +141,14 @@ app.layout = html.Div(
         className = 'six columns')],
     className = 'row') ])
 )
+
+"""
+
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
